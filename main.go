e453ee602db1b744ef0f0bb91039fe9c3606b275
n0tb0dy <n0tb0dy@gmail.com>
2024-07-16 00:15:23 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	// Проверяем наличие аргументов командной строки
+	if len(os.Args) < 3 {
+		log.Fatal("Usage: go run main.go <port> <response message>")
+	}
+
+	// Получаем порт и сообщение из аргументов командной строки
+	port := os.Args[1]
+	responseMessage := os.Args[2]
+
+	// Обработчик запросов
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Логируем информацию о запросе
+		fmt.Printf("Received request from IP: %s\n", r.RemoteAddr)
+		fmt.Println("Request headers:")
+		for name, values := range r.Header {
+			for _, value := range values {
+				fmt.Printf("%s: %s\n", name, value)
+			}
+		}
+
+		// Отправляем ответ
+		fmt.Fprintln(w, responseMessage)
+	})
+
+	// Выводим информацию о запущенном сервере
+	fmt.Printf("Server is listening on port %s\n", port)
+
+	// Запускаем сервер
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}

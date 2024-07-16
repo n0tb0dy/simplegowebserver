c5b8d64b35723997d966a673667594cb7400fc93
n0tb0dy <n0tb0dy@gmail.com>
2024-07-16 08:41:04 +0300

    fix app 03

--- simplegowebserver.go
+++ simplegowebserver.go
@@ -1,28 +1,35 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "log"
-    "net/http"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-    port := flag.String("port", "8080", "Port to listen on")
-    message := flag.String("message", "Hello, World!", "Message to respond with")
-    showRequest := flag.Bool("m", false, "Show request details")
-    flag.Parse()
+	// Определение флагов командной строки
+	port := flag.String("port", "8080", "Port to listen on")
+	message := flag.String("message", "Hello, World!", "Message to respond with")
+	monitor := flag.Bool("m", false, "Monitor and log client IP and request details")
+	flag.Parse()
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        clientIP := r.RemoteAddr
-        fmt.Fprintln(w, *message)
-        if *showRequest {
-            fmt.Printf("Client IP: %s\nRequest: %s\n", clientIP, r)
-        } else {
-            fmt.Printf("Client IP: %s\n", clientIP)
-        }
-    })
+	if flag.NArg() > 0 {
+		fmt.Println("Usage: simplegowebserver -port <port> -message <message> [-m]")
+		os.Exit(1)
+	}
 
-    fmt.Printf("Server is listening on port %s...\n", *port)
-    log.Fatal(http.ListenAndServe(":"+*port, nil))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		clientIP := r.RemoteAddr
+		fmt.Fprintln(w, *message)
+		if *monitor {
+			fmt.Printf("Client IP: %s\nRequest: %s\n", clientIP, r)
+		} else {
+			fmt.Printf("Client IP: %s\n", clientIP)
+		}
+	})
+
+	fmt.Printf("Server is listening on port %s...\n", *port)
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
